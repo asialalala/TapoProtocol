391629a2aa114b812eeedc840749a8717a21630e
Joasia Klasik <asklas@op.pl>
2024-04-28 12:09:24 +0200

    main with cli function

---
+++ main.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+
+enum deviceType {
+    smartBulb
+};
+int main(int argc, char *argv[])
+{
+
+}
+void cli()
+{
+
+}
+
+void asyncWrappedDevice()
+{
+
+}
+
+void wifi()
+{
+
+}
+
+void scan()
+{
+    
+}
+
+void join()
+{
+
+}
+
+void discover()
+{
+
+}
+
+void printUnsupported()
+{
+
+}
+void printDiscovered()
+{
+
+}
+
+void echoDictionary()
+{
+
+}
+
+void echoDictionaryInfo()
+{
+
+}
+
+void findHostFromAlias()
+{
+
+}
+
+void sysinfo()
+{
+
+}
+
+void echoFeatures()
+{
+
+}
+
+void echoAllFeatures()
+{
+    
+}
+
+void state()
+{
+
+}
+
+void alias()
+{
+
+}
+
+void rawCommand()
+{
+
+}
+
+void cmdCommand()
+{
+
+}
+
+void emeter()
+{
+
+}
+
+void usage()
+{
+
+}
+
+void brightness()
+{
+
+}
+
+void temperature()
+{
+
+}
+
+void effect()
+{
+
+}
+
+void hsv()
+{
+
+}
+
+void led()
+{
+
+}
+void time()
+{
+
+}
+
+void on()
+{
+    
+}
+void off()
+{
+    
+}
+
+void toggle()
+{
+    
+}
+
+void reboot()
+{
+    
+}
+
+void schedule()
+{
+    
+}
+
+void scheduleList()
+{
+    
+}
+
+void deletRule()
+{
+
+}
+
+void presents()
+{
+    
+}
+
+void presentsList()
+{
+    
+}
+
+void presentsModify()
+{
+    
+}
+
+void turnOnBehavior()
+{
+    
+}
+
+void updateCredentials()
+{
+    
+}
+
